aac7d0e60c2adfe6e5797e7969e6daa8d7f6ada9
Pia Lusth <162367082+Peijo77@users.noreply.github.com>
2024-06-10 20:21:02 +0200

    Add project files.

---
+++ Controllers/DoctorController.cs
@@ -0,0 +1,20 @@
+﻿using FeverCheckControl.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FeverCheckControl.Controllers
+{
+    public class DoctorController : Controller
+    {
+        public IActionResult FeverCheck()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CheckTemperature(float temperature) 
+        { 
+            string message = TemperatureUtility.CheckFever(temperature);
+            return Json(message);
+        }
+    }
+}
---
+++ Models/TemperatureUtility.cs
@@ -0,0 +1,23 @@
+﻿
+namespace FeverCheckControl.Models
+{
+    public static class TemperatureUtility
+    {
+        public static string CheckFever(float temperature)
+        {
+            if (temperature >=37.5) 
+            {
+                return "YOU HAVE FEVER";
+            }
+            else if (temperature <=35)
+            {
+                return "YOU MIGHT HAVE HYPOTHERMIA";
+            }
+            else 
+            {
+                return "YOUR TEMPERATURE IS NORMAL";
+            }
+        }
+    }
+
+}
